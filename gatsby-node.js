6ab5267656906f8a173f51b433bbf0a1981969fa
David <sanderdj90@gmail.com>
2021-10-20 16:41:23 -0600

    poc

--- gatsby-node.js
+++ gatsby-node.js
@@ -1,7 +1,54 @@
-/**
- * Implement Gatsby's Node APIs in this file.
- *
- * See: https://www.gatsbyjs.com/docs/node-apis/
- */
+const fruits = {
+  category: "fruit",
+  types: ["lemons", "pineapple", "kimi", "starfruit"],
+}
+const veggies = {
+  category: "vegetable",
+  types: ["broccoli", "carrots", "peas", "spinach"],
+}
 
-// You can delete this file if you're not using it
+exports.sourceNodes = async ({
+  actions,
+  createNodeId,
+  createContentDigest,
+}) => {
+  fruits.types.forEach((fruit, i) => {
+    const node = {
+      category: fruits.category,
+      name: fruit,
+      id: createNodeId(i),
+      internal: {
+        type: fruits.category,
+        contentDigest: createContentDigest(fruit),
+      },
+    }
+    actions.createNode(node)
+  })
+  veggies.types.forEach((veggie, i) => {
+    const node = {
+      category: veggies.category,
+      name: veggie,
+      id: createNodeId(i),
+      internal: {
+        type: veggies.category,
+        contentDigest: createContentDigest(veggie),
+      },
+    }
+    actions.createNode(node)
+  })
+}
+
+exports.onCreateWebpackConfig = ({ stage, loaders, actions }) => {
+  if (stage === "build-html") {
+    actions.setWebpackConfig({
+      module: {
+        rules: [
+          {
+            test: /isotope-layout/,
+            use: loaders.null(),
+          },
+        ],
+      },
+    })
+  }
+}
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,29 +1,71 @@
-import * as React from "react"
-import { Link } from "gatsby"
-import { StaticImage } from "gatsby-plugin-image"
-
+import React, { useRef } from "react"
+import Isotope from "isotope-layout"
 import Layout from "../components/layout"
-import Seo from "../components/seo"
+import { useStaticQuery, graphql } from "gatsby"
 
-const IndexPage = () => (
-  <Layout>
-    <Seo title="Home" />
-    <h1>Hi people</h1>
-    <p>Welcome to your new Gatsby site.</p>
-    <p>Now go build something great.</p>
-    <StaticImage
-      src="../images/gatsby-astronaut.png"
-      width={300}
-      quality={95}
-      formats={["auto", "webp", "avif"]}
-      alt="A Gatsby astronaut"
-      style={{ marginBottom: `1.45rem` }}
-    />
-    <p>
-      <Link to="/page-2/">Go to page 2</Link> <br />
-      <Link to="/using-typescript/">Go to "Using TypeScript"</Link>
-    </p>
-  </Layout>
-)
+const IndexPage = () => {
+  const isotope = React.useRef()
+  // store the filter keyword in a state
+  const [filterKey, setFilterKey] = React.useState("fruits")
+
+  React.useEffect(() => {
+    isotope.current = new Isotope(".filter-container", {
+      itemSelector: ".item",
+      layoutMode: "fitRows",
+    })
+    // cleanup
+    return () => isotope.current.destroy()
+  }, [])
+
+  // handling filter key change
+  React.useEffect(() => {
+    filterKey === "*"
+      ? isotope.current.arrange({ filter: `*` })
+      : isotope.current.arrange({ filter: `.${filterKey}` })
+  }, [filterKey])
+
+  const handleFilterKeyChange = key => () => setFilterKey(key)
+
+  const data = useStaticQuery(graphql`
+    query fruitsAndVeggies {
+      fruits: allFruit {
+        nodes {
+          id
+          name
+          category
+        }
+      }
+      veggies: allVegetable {
+        nodes {
+          id
+          name
+          category
+        }
+      }
+    }
+  `)
+
+  const filters = ["fruit", "vegetable", "*"]
+
+  const filterBtns = filters.map(el => (
+    <button onClick={handleFilterKeyChange(el)} id={el}>
+      {el}
+    </button>
+  ))
+  const fruitTiles = data.fruits.nodes.map(el => (
+    <div className={`item all ${el.category} ${el.name}`}>{el.name}</div>
+  ))
+  const veggieTiles = data.veggies.nodes.map(el => (
+    <div className={`item all ${el.category} ${el.name}`}>{el.name}</div>
+  ))
+
+  const tiles = [...veggieTiles, ...fruitTiles]
+  return (
+    <Layout>
+      {filterBtns}
+      <div className="filter-container">{tiles}</div>
+    </Layout>
+  )
+}
 
 export default IndexPage
